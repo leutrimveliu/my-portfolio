8d2d8f7b455010d03e85120dd3139dc28c0fa0b6
Leutrim <58770472+leutrimveliu@users.noreply.github.com>
2020-10-05 18:47:49 +0200

    navbar-dev

---
+++ src/components/Dropdown.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import "./Dropdown.css";
+
+function Dropdown() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+  return (
+    <>
+      <ul
+        onClick={handleClick}
+        className={click ? "dropdown-menu clicked" : "dropdown-menu"}
+      ></ul>
+    </>
+  );
+}
+
+export default Dropdown;
---
+++ src/components/Header/Navbar.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  return (
+    <>
+      <nav className="navbar">
+        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+          EPIC
+          <i class="fab fa-firstdraft" />
+        </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas fa-bars"} />
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/resume" className="nav-links" onClick={closeMobileMenu}>
+              Resume
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/aboutme" className="nav-links" onClick={closeMobileMenu}>
+              About Me
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/projects"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Projects
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/contact" className="nav-links" onClick={closeMobileMenu}>
+              Contact
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/sign-up"
+              className="nav-links-mobile"
+              onClick={closeMobileMenu}
+            >
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Navbar;
