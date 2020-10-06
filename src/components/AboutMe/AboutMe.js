4d0a79bdfb48d5e7c36ebe4d5355d7ae911309ce
Leutrim <58770472+leutrimveliu@users.noreply.github.com>
2020-10-06 13:37:19 +0200

    navigation-banner-done

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./AboutMe.css";
+
+const AboutMe = () => {
+  return <div className="mediaQuery">About Me</div>;
+};
+export default AboutMe;
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return <div className="mediaQuery">Footer</div>;
+};
+export default Footer;
